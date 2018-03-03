0c7f0b0ebaa0f4369d06d769f64d6147108b30e1
at-thiennguyen <nguyenthanhthien2031@gmail.com>
2018-03-03 15:44:04 +0700

    First commit

---
+++ public/js/main/app.js
@@ -0,0 +1,7 @@
+var app = angular.module("app.todos", []);
+
+app.controller("todoController", ['$scope', function ($scope) {
+    
+    $scope.appName = "Any things !!";
+
+}])
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/user', function (req, res, next) {
+  var a = req.query.abc
+
+  res.json(
+    { User: a }
+  )
+});
+
+router.post('/user', function (req, res, next) {
+  var a = req.body.hihi
+
+  res.send(
+    { User: a }
+  )
+});
+
+module.exports = router;
