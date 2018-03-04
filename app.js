d5b32d68ebce062275ccdc9ec6515c761b96f20b
at-thiennguyen <nguyenthanhthien2031@gmail.com>
2018-03-05 00:37:57 +0700

    Fix bug Route

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not % Found');
   err.status = 404;
   next(err);
 });
